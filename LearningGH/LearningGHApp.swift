546bbb1b12da457566e1fae5f83dcff9818f6ce2
Simon Karlsson <Karlssonsimon11@gmail.com>
2022-01-27 19:05:15 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nina  Jonsson on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
